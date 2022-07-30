d54bb0334db98401767595d3e35b38009cd8d672
Hafiz Muhammad Aamir <aamirorbit@gmail.com>
2022-07-30 05:47:54 -0700

    Fixed initial commit error

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Web3 Twitter Clone App</title>
         <meta name="description" content="A basic Web3 Twitter clone app using NextJS and firebase" />
